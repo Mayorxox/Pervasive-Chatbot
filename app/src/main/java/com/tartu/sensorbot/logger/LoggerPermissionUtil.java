897055a45543ef2b9c486e6151615dac5d563e04
Tarlan Hasanli <tarlanhasanli9@gmail.com>
2023-10-04 15:21:20 +0300

    refactors logger permission util

--- app/src/main/java/com/tartu/sensorbot/logger/LoggerPermissionUtil.java
+++ app/src/main/java/com/tartu/sensorbot/logger/LoggerPermissionUtil.java
@@ -1,6 +1,5 @@
 package com.tartu.sensorbot.logger;
 
-import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
 import android.app.AlertDialog;
 import android.app.AppOpsManager;
@@ -25,17 +24,10 @@ public class LoggerPermissionUtil {
       startActivity(Settings.ACTION_USAGE_ACCESS_SETTINGS);
     }
 
-    // Check if the AccessibilityService is enabled and if not, guide the user to enable it
-    if (!isAccessibilityServiceEnabled(LogActivityService.class)) {
-      new AlertDialog.Builder(context)
-          .setTitle("Enable Accessibility Service")
-          .setMessage("For the user study, please enable the UserActivityService in the accessibility settings.")
-          .setPositiveButton("Go to Settings", (dialog, which) -> {
-            // Direct the user to the accessibility settings
-            startActivity(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-          })
-          .setNegativeButton("Cancel", null)
-          .show();
+    boolean isAccessibilityServiceEnabled = getEnabledServices().stream().anyMatch(
+            service -> isAccessibilityServiceEnabled(service, LogActivityService.class.getName()));
+    if (!isAccessibilityServiceEnabled) {
+      showEnableAccessibilityServiceAlert();
     }
   }
 
@@ -43,23 +35,32 @@ public class LoggerPermissionUtil {
     context.startActivity(new Intent(actionAccessibilitySettings));
   }
 
-  public boolean hasUsageStatsPermission() {
+  private boolean hasUsageStatsPermission() {
     AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
     int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, android.os.Process.myUid(), context.getPackageName());
     return mode == AppOpsManager.MODE_ALLOWED;
   }
 
-  public boolean isAccessibilityServiceEnabled(Class<? extends AccessibilityService> service) {
+  private List<AccessibilityServiceInfo> getEnabledServices() {
     AccessibilityManager am = (AccessibilityManager) context.getSystemService(Context.ACCESSIBILITY_SERVICE);
-    List<AccessibilityServiceInfo> enabledServices = am.getEnabledAccessibilityServiceList(
-        AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
-    for (AccessibilityServiceInfo enabledService : enabledServices) {
-      ServiceInfo enabledServiceInfo = enabledService.getResolveInfo().serviceInfo;
-      if (enabledServiceInfo.packageName.equals(context.getPackageName()) && enabledServiceInfo.name.equals(service.getName())) {
-        return true;
-      }
-    }
-    return false;
+    return am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_ALL_MASK);
   }
 
+  private boolean isAccessibilityServiceEnabled(AccessibilityServiceInfo enabledService, String serviceName) {
+    ServiceInfo enabledServiceInfo = enabledService.getResolveInfo().serviceInfo;
+    return enabledServiceInfo.packageName.equals(context.getPackageName())
+        && enabledServiceInfo.name.equals(serviceName);
+  }
+
+  private void showEnableAccessibilityServiceAlert() {
+    new AlertDialog.Builder(context)
+        .setTitle("Enable Accessibility Service")
+        .setMessage("For the user study, please enable the UserActivityService in the accessibility settings.")
+        .setPositiveButton("Go to Settings", (dialog, which) -> {
+          // Direct the user to the accessibility settings
+          startActivity(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        })
+        .setNegativeButton("Cancel", null)
+        .show();
+  }
 }
